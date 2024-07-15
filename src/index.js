394ab2bcb6b76569bf924e0ab06e56e365557d0c
Danil <gul.danil99@gmail.com>
2024-07-15 17:04:52 +0300

    regregreg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-
+// blank
 const bootStrap = async () => {
   await initMongoConnection();
   setupServer();
