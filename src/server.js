7c96445293577b9662a22ccf042304d343f991de
Danil <gul.danil99@gmail.com>
2024-07-21 03:58:39 +0300

    done

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import authRouter from './routers/auth.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
 import { PUBLIC_DIR } from './constants/index.js';
+import swaggerDocs from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 const PORT = Number(env('PORT', '3000'));
@@ -26,6 +27,7 @@ export const setupServer = () => {
   app.use(cookieParser());
   app.use(express.json());
   app.use(express.static(PUBLIC_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
